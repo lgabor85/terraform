ca338d6426e86c1ceee2db19d61ded9d71c19751
Gabor Lakatos <gaborl@topdesk.com>
2025-06-12 13:17:47 +0200

    Rename Terraform files to quickstart folder

---
+++ quickstart/managedIdentity/variables.tf
@@ -0,0 +1,16 @@
+variable "subscription_id" {
+  type = string
+  default = "21746260-d74d-4799-b99f-0c4cf7080e7d"
+  
+}
+
+variable "resource_suffix" {
+  type = string
+  default = "3td4x"
+}
+
+variable "location" {
+  type = string
+  default = "westeurope"
+}
+
