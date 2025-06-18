227409d7a04a46594ff7ee1d6ed3a534171a8ddb
Gabor Lakatos <gaborl@topdesk.com>
2025-06-18 11:27:44 +0200

    modified resource names

--- quickstart/anf/main.tf
+++ quickstart/anf/main.tf
@@ -4,7 +4,7 @@ resource "random_pet" "rg_name" {
 }
 
 # Create Resource Group
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "anf-rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
@@ -19,7 +19,7 @@ resource "random_string" "name" {
 }
 
 # Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "anf-vnet" {
   name                = "vnet-${random_string.name.result}"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 # Create Subnet for NetApp
-resource "azurerm_subnet" "subnet" {
+resource "azurerm_subnet" "anf-subnet" {
   name                 = "subnet-${random_string.name.result}"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
--- quickstart/anf/outputs.tf
+++ quickstart/anf/outputs.tf
@@ -1,15 +1,15 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.anf-rg.name
 }
 
 output "netapp_account_name" {
-  value = azurerm_netapp_account.account.name
+  value = azurerm_netapp_account.anf-account.name
 }
 
 output "netapp_pool_name" {
-  value = azurerm_netapp_pool.pool.name
+  value = azurerm_netapp_pool.anf-pool.name
 }
 
 output "netapp_volume_name" {
-  value = azurerm_netapp_volume.volume.name
+  value = azurerm_netapp_volume.anf-volume.name
 }
