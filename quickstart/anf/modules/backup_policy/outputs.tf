915aa4d8ac8842daf587d197ba494cde819c68de
Gabor Lakatos <gaborl@topdesk.com>
2025-06-19 14:45:50 +0200

    corrected outpus value

--- quickstart/anf/modules/backup_policy/outputs.tf
+++ quickstart/anf/modules/backup_policy/outputs.tf
@@ -1,4 +1,4 @@
 output "policy_id" {
-  value       = azapi_resource.backup_policy.id
+  value       = azapi_resource.anf_daily_only_backup_policy.id
   description = "The resource ID of the created NetApp backup policy."
 }
